3999eea3af2229e8113ad7a2d287cbe19fd60208
fisker Cheung <lionkay@gmail.com>
2021-07-20 13:56:38 +0800

    Tweak

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,14 +19,6 @@ module.exports = {
   overrides: [
     {
       files: ['index.js', 'import.js', 'tests/main.js'],
-      extends: [
-        'plugin:es/no-2019',
-        'plugin:es/no-2018',
-        'plugin:es/no-2017',
-        'plugin:es/no-2016',
-        'plugin:es/no-2015',
-        'plugin:es/no-5',
-      ],
       rules: {
         'no-var': 'off',
         strict: 'off',
